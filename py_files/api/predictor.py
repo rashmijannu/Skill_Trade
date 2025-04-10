d93415ba4832f9959b8a99d8825a6e9a5156074a
Ayush <asharma7588@gmail.com>
2025-04-10 17:25:28 +0530

    chnaged the predictor file

--- py_files/api/predictor.py
+++ py_files/api/predictor.py
@@ -4,22 +4,19 @@ import joblib
 import numpy as np
 import os
 
-
-
 app = FastAPI()
 
 # Load model
-# model = joblib.load("../train_modal/price_model.pkl")
 model = joblib.load(os.path.join(os.path.dirname(__file__), '..', 'train_modal', 'price_model.pkl'))
 
 # Input schema
 class InputData(BaseModel):
     serviceType: int
-    city: int
+    city: str 
     time: int
 
 @app.post("/predict")
 def predict_price(data: InputData):
-    input_array = np.array([[data.serviceType, data.city, data.time]])
+    input_array = [[data.serviceType, data.city, data.time]]  
     prediction = model.predict(input_array)
-    return {"predictedprice": int(prediction[0])}
+    return {"predictedprice": max(int(prediction[0]), 0)} 
