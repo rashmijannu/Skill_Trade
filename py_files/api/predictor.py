387cea3c95c2f29d2e8f401da8c22432aa75d209
Ayush <asharma7588@gmail.com>
2025-04-10 16:53:44 +0530

    added requirements.txt

--- py_files/api/predictor.py
+++ py_files/api/predictor.py
@@ -2,11 +2,15 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import numpy as np
+import os
+
+
 
 app = FastAPI()
 
 # Load model
-model = joblib.load("../train_modal/price_model.pkl")
+# model = joblib.load("../train_modal/price_model.pkl")
+model = joblib.load(os.path.join(os.path.dirname(__file__), '..', 'train_modal', 'price_model.pkl'))
 
 # Input schema
 class InputData(BaseModel):
