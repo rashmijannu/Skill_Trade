20203dc4065378fd09637778f65dbc2fcb4acdf1
Ayush <asharma7588@gmail.com>
2025-04-13 18:01:21 +0530

    added cors in prediction api

--- py_files/api/predictor.py
+++ py_files/api/predictor.py
@@ -3,17 +3,25 @@ from pydantic import BaseModel
 import joblib
 import pandas as pd
 import os
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Load trained model
 model_path = os.path.join(os.path.dirname(__file__), '..', 'train_modal', 'price_model.pkl')
 model = joblib.load(model_path)
 
-# Request input format 
 class PriceInput(BaseModel):
     serviceType: int
-    city: int  
+    city: int
     time: int
 
 @app.post("/predict")
@@ -21,11 +29,11 @@ def predict_price(data: PriceInput):
     # Create a DataFrame with the input
     input_df = pd.DataFrame([{
         "serviceType": data.serviceType,
-        "city_code": data.city,   
+        "city_code": data.city,
         "time": data.time
     }])
 
-    # Predict the price
+    
     prediction = model.predict(input_df)
 
     # Return the prediction
